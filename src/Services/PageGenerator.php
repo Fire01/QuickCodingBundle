169f04ba1022ff6c77b0a2f1fe1a4916d26b6edc
Ryan Firdaus <Riyanfirdaus90@yahoo.co.id>
2019-09-11 00:12:38 +0700

    Add support native view

--- src/Services/PageGenerator.php
+++ src/Services/PageGenerator.php
@@ -3,8 +3,15 @@ namespace Fire01\QuickCodingBundle\Services;
 
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Knp\Component\Pager\PaginatorInterface;
 
 class PageGenerator extends AbstractController { 
+    
+    private $knpPaginator;
+    
+    function __construct(PaginatorInterface $knpPaginator){
+        $this->knpPaginator = $knpPaginator;
+    }
     
     function generate($configuration){
         $request = $this->get('request_stack')->getCurrentRequest();
@@ -22,14 +29,34 @@ class PageGenerator extends AbstractController {
                 return $this->generateForm($configuration['title'], $configuration['entity'], $configuration['form'], $id, true);
                 break;
             case 'delete':
-                return $this->remove($configuration['entity'], $id);
+                return $this->remove($configuration['entity'], $id, $request->attributes->get('_route'));
                 break;
         }
         
-        return $this->generateView($configuration['title'], $configuration['entity'], $configuration['view']);
+        if($configuration['list'] == 'datatables'){
+            return $this->generateDataTables($configuration['title'], $configuration['entity'], $configuration['view']);
+        }else{
+            return $this->generateView($configuration['title'], $configuration['entity'], $configuration['view']);
+        }
+    }
+    
+    function generateView($title, $entityID, $view, $perPage=25){
+        $request = $this->get('request_stack')->getCurrentRequest();
+        
+        $order = [];
+        if($request->query->get('order')){
+            $order = explode("-", $request->query->get('order'));
+        }else{
+            if(isset($view[0])){
+                $order = [$view[0]['name'], 'Asc'];
+            }
+        }
+        
+        $data =  $this->data($entityID, $order, $request->query->getInt('page', 1), $perPage, $request->query->get('search'));
+        return $this->render('@quick_coding.view/component/view.html.twig', ["title" => $title, "column" => $view, "path" => $request->attributes->get('_route'), "order" => $order, "data" => $data]);
     }
    
-    function generateView($title, $entityID, $view){
+    function generateDataTables($title, $entityID, $view){
         $request = $this->get('request_stack')->getCurrentRequest();
         $path = $this->generateUrl($request->attributes->get('_route'), []);
         $query = $request->query->all();
@@ -45,7 +72,7 @@ class PageGenerator extends AbstractController {
             ], JsonResponse::HTTP_OK);
         }
         
-        return $this->render('@quick_coding.view/component/view.html.twig', ["title" => $title, "column" => $view, "path" => $path]);
+        return $this->render('@quick_coding.view/component/dataTables.html.twig', ["title" => $title, "column" => $view, "path" => $path]);
     }
     
     function generateForm($title, $entityID, $formId, $id=null, $edit=true){
@@ -72,13 +99,31 @@ class PageGenerator extends AbstractController {
         ]);
     }
     
-    function remove($entityID, $id){
+    function remove($entityID, $id, $route){
         $item = $this->getDoctrine()->getRepository($entityID)->find($id);
         $em = $this->getDoctrine()->getManager();
         $em->remove($item);
         $em->flush();
         
-        return $this->json(['success' => true], JsonResponse::HTTP_OK);
+        if($this->get('request_stack')->getCurrentRequest()->isXmlHttpRequest()){
+            return $this->json(['success' => true], JsonResponse::HTTP_OK);
+        }else{
+            return $this->redirectToRoute($route);
+        }
+    }
+    
+    function data($entityID, $order, $page, $perPage, $search){
+        $repository = $this->getDoctrine()->getRepository($entityID);
+        
+        $data = $repository->createQueryBuilder('t');
+        
+        if($search){
+            foreach($column as $col){$data->orWhere('t.' . $col['name'] . ' LIKE :' . $col['name'])->setParameter( $col['name'], '%' . $search . '%');}
+        }
+        
+        if(count($order) == 2){$data->orderBy('t.' . $order[0], $order[1]);}
+        
+        return $this->knpPaginator->paginate($data->getQuery()->getArrayResult(), $page, $perPage);
     }
     
     function dataTable($entityID, $column, $orders, $length, $start, $search){
