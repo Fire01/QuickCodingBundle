28bc8267f6812f159813d115882df008892b33a1
Ryan Firdaus <Riyanfirdaus90@yahoo.co.id>
2019-09-21 20:19:35 +0700

    Add Helpers Class

---
+++ src/Services/Helpers.php
@@ -0,0 +1,14 @@
+<?php 
+namespace Fire01\QuickCodingBundle\Services;
+
+class Helpers {
+    
+    public static function dateFormater(\DateTimeInterface $obj){
+        if($obj && $obj->format('Y-m-d H:i:s') != '-0001-11-30 00:00:00'){
+            $obj->add(date_interval_create_from_date_string('-7 hours'));
+            return ['date' => $obj->format('Y-m-d') . 'T' . $obj->format('H:i:s') . 'Z'];
+        }
+        
+        return ['date' => null];
+    }
+}
