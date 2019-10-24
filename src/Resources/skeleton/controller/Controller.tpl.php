9b6cad16f598a2c3f70c6af423751d94faffa033
Ryan Firdaus <Riyanfirdaus90@yahoo.co.id>
2019-10-24 16:48:38 +0700

    # Change Maker to new config

--- src/Resources/skeleton/controller/Controller.tpl.php
+++ src/Resources/skeleton/controller/Controller.tpl.php
@@ -8,23 +8,19 @@ use Fire01\QuickCodingBundle\Builder;
 
 class <?= $class_name; ?> extends <?= $parent_class_name; ?><?= "\n" ?>
 {
-
-	private $config = [
-        'title'	=> '<?= ucfirst($route_name) ?>',
-        'entity'=> '<?= $entity_name ?>',
-        'form'	=> '<?= $form_name ?>',
-        'column'=> [
-<?php foreach($columns as $column){ ?>
-			['title' => '<?= $column['title'] ?>', 'name' => '<?= $column['name'] ?>'],
-<?php } ?>
-        ]
-    ];
-    
     /**
      * @Route("/qc<?= $route_path ?>/{action}/{id}", requirements={"id"="\d+"}, name="quick_coding_<?= $route_name ?>", methods="GET|POST")
      */
     public function qc_generator(Builder $builder, $action=null, $id=null){
-        return $builder->setConfig(array_merge($this->config, ['action' => $action]))->build();
+        return $builder->setConfig([
+            'title'	=> '<?= $title ?>',
+            'entity'=> '<?= $entity_name ?>',
+            'form'	=> '<?= $form_name ?>',
+            'view'=> [
+                'select' => [<?php foreach($columns as $column){ ?>	'<?= $column['title'] ?>' => '<?= $column['name'] ?>',	<?php } ?>],
+            ],
+            'action' => $action
+        ])->build();
     }
     
 }
