2bce83a931415d83607175923b4cf64eea746248
Ryan Firdaus <riyanfirdaus90@yahoo.co.id>
2019-10-26 23:11:12 +0700

    # Multifile : Image

--- src/FormType/MultifileType.php
+++ src/FormType/MultifileType.php
@@ -3,13 +3,21 @@ namespace Fire01\QuickCodingBundle\FormType;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\Form\FormInterface;
 
 class MultifileType extends AbstractType
 {
+    public function buildView(FormView $view, FormInterface $form, array $options) {
+        
+        $view->vars['file_type'] = $options['fileType'];
+    }
+    
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
             'multiple' => true,
+            'fileType' => null,
             'mapped' => false,
             'attr' => ['allow' => null],
         ]);
